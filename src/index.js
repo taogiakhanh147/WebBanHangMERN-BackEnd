8fca5db93caafbc2d978c01eb9a1cfec9dec19e7
TaoGiaKhanh147 <taogiakhanh@gmail.com>
2024-09-08 15:53:40 +0700

    update system admin

--- src/index.js
+++ src/index.js
@@ -11,8 +11,11 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors())
+
+// upload image
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
+
 app.use(bodyParser.json())
 app.use(cookieParser())
 routes(app);
